b3a802924b9aef6a0c5b770f80d5bd1c52ed9f59
RaulParraM <raosprat@hotmail.com>
2019-02-25 20:30:13 +0100

    LSP commit

---
+++ LSP/LSP/Programador.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSP
+{
+    abstract class ProgramadorJunior
+    {
+        public virtual int sueldo()
+        {
+            return 2000;
+        }
+    }
+
+    class Mediocre : ProgramadorJunior
+    {
+        public override int sueldo()
+        {
+            return 1500;
+        }
+    }
+
+    class Brillante : ProgramadorJunior
+    {
+        public override int sueldo()
+        {
+            return 2500;
+        }
+    }
+
+
+
+}
