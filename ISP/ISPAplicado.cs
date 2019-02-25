358f3673e46131d8079dc0e11fb94084daebf96f
RaulParraM <raosprat@hotmail.com>
2019-02-25 21:07:16 +0100

    ISP commit

---
+++ ISP/ISPAplicado.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISP
+{
+    interface IEnviarSolicitudDeAmistad
+    {
+        void EnviarSolicitudDeAmistad();
+    }
+
+    interface IBanear
+    {
+        void Banear();
+    }
+
+    interface ICambiarApariencia
+    {
+        void CambiarApariencia();
+    }
+
+    class Admin : ICambiarApariencia, IBanear, IEnviarSolicitudDeAmistad
+    {
+        public void EnviarSolicitudDeAmistad()
+        {
+
+        }
+
+        public void Banear()
+        {
+
+        }
+        public void CambiarApariencia()
+        {
+
+        }
+    }
+
+    class Prenium : ICambiarApariencia, IEnviarSolicitudDeAmistad
+    {
+        public void EnviarSolicitudDeAmistad()
+        {
+
+        }
+
+        public void CambiarApariencia()
+        {
+
+        }
+
+    }
+
+    class Normal : IEnviarSolicitudDeAmistad
+    {
+        public void EnviarSolicitudDeAmistad()
+        {
+
+        }
+    }
+}
---
+++ ISP/ISPNoAplicado.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISP
+{
+    interface Funcionalidades
+    {
+        void EnviarSolicitudDeAmistad();
+        void Banear();
+        void CambiarApariencia();
+    }
+
+    class Admin : Funcionalidades
+    {
+        public void EnviarSolicitudDeAmistad()
+        {
+
+        }
+        public void Banear()
+        {
+
+        }
+        public void CambiarApariencia()
+        {
+
+        }
+    }
+
+    class Prenium : Funcionalidades
+    {
+        public void EnviarSolicitudDeAmistad()
+        {
+
+        }
+        public void Banear()
+        {
+            throw new Exception("No se puede usar");
+        }
+        public void CambiarApariencia()
+        {
+
+        }
+    }
+
+    class Normal : Funcionalidades
+    {
+        public void EnviarSolicitudDeAmistad()
+        {
+
+        }
+        public void Banear()
+        {
+            throw new Exception("No se puede usar");
+        }
+        public void CambiarApariencia()
+        {
+            throw new Exception("No se puede usar");
+        }
+    }
+
+}
