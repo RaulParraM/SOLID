1f2eb89e5245ea397fa5ecbbd693d764429efe78
RaulParraM <raosprat@hotmail.com>
2019-02-25 14:02:39 +0100

    OSP commit

---
+++ OSP/OSPAplicado.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSP
+{
+    class UsuarioAdmin
+    {
+        public string nombreUsuario { get; set; }
+        public int edad { get; set; }
+    }
+
+    class UsuarioComun
+    {
+        public string nombreUsuario { get; set; }
+        public int edad { get; set; }
+    }
+
+    class Caracteristicas
+    {
+        public bool UsuarioPuedeBanear(Usuario usuario)
+        {
+            //Comprovaciones
+            return true;
+        }
+    }
+}
---
+++ OSP/OSPNoAplicado.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSP
+{
+    class UsuarioAdmin
+    {
+        public string nombreUsuario { get; set; }
+        public int edad { get; set; }
+    }
+
+    class UsuarioComun
+    {
+        public string nombreUsuario { get; set; }
+        public int edad { get; set; }
+    }
+
+    class Caracteristicas
+    {
+        public bool UsuarioAdminPuedeBanear(UsuarioAdmin usuario)
+        {
+            //Comprovaciones
+            return true;
+        }
+
+        public bool UsuarioComunPuedeBanear(UsuarioComun usuario)
+        {
+            //Comprovaciones
+            return true;
+        }
+    }
+}
